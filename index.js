94fdad9bdbb95e2b36d3e040385e9341287cfb20
linabreu <linabreu@outlook.com>
2024-06-14 20:23:41 -0700

    ???

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const cors = require('cors');
-app.use(cors());
+//const cors = require('cors');
+//app.use(cors());
 
 //const morgan = require('morgan');
 //app.use(morgan(':method  :response-time ms :url :body'));
